f61d5f7e41310bb83782b72b22df6630330dcbf1
Williams Hernández <hernandw@gmail.com>
2023-09-01 21:22:59 -0400

    :tada: componente button

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './components/Button'
+
+const App = () => {
+  return (
+    <div>
+      <Button color="success" />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = ({color}) => {
+  return (
+    <div>
+          <button className={`btn btn-${color}`}>Enviar</button>
+    </div>
+  )
+}
+
+export default Button
